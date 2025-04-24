555aa6606d809306af679c374671d1288a0733fb
IchbinMrNook <jonas.l.raab@gmail.com>
2025-04-24 14:30:56 +0200

    A

--- Env_new/allow.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Setzt die PowerShell-Ausführungsrichtlinie auf RemoteSigned
-powershell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
-echo PowerShell-Ausführungsrichtlinie wurde auf RemoteSigned gesetzt.
--- Env_old/restrict.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Setzt die PowerShell-Ausführungsrichtlinie auf Undefined
-powershell -Command "Set-ExecutionPolicy Undefined -Scope CurrentUser -Force"
-echo PowerShell-Ausführungsrichtlinie wurde auf Undefined gesetzt.
